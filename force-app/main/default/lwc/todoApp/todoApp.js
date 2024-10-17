e39292dc5c2c1566e2f7e612b0ea650affdb5ae3
codingtoc <codingtoc@gmail.com>
2024-10-17 21:30:20 +0900

    todoList 배열 데이터 추가

--- force-app/main/default/lwc/todoApp/todoApp.js
+++ force-app/main/default/lwc/todoApp/todoApp.js
@@ -1,22 +1,27 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class TodoApp extends LightningElement {
-  todoList = [
-    {
-      id: 1,
-      name: "Task 1"
-    },
-    {
-      id: 2,
-      name: "Task 2"
-    },
-    {
-      id: 3,
-      name: "Task 3"
-    },
-    {
-      id: 4,
-      name: "Task 4"
+  @track
+  todoList = [];
+
+  newTodoItem = "";
+
+  updateNewTodoItem(event) {
+    this.newTodoItem = event.target.value;
+  }
+
+  addTodoItemToList(event) {
+    if (this.newTodoItem.trim() === "") {
+      this.newTodoItem = "";
+      return;
     }
-  ];
+    this.todoList.push({
+      id:
+        this.todoList.length > 0
+          ? this.todoList[this.todoList.length - 1].id + 1
+          : 1,
+      name: this.newTodoItem
+    });
+    this.newTodoItem = "";
+  }
 }
