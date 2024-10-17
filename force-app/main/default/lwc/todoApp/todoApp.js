24a649a185712e638664f604ba2089847ba00cc3
codingtoc <codingtoc@gmail.com>
2024-10-17 19:27:05 +0900

    todoList 배열 데이터 바인딩 및 렌더링

--- force-app/main/default/lwc/todoApp/todoApp.js
+++ force-app/main/default/lwc/todoApp/todoApp.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class TodoApp extends LightningElement {}
+export default class TodoApp extends LightningElement {
+  todoList = [
+    {
+      id: 1,
+      name: "Task 1"
+    },
+    {
+      id: 2,
+      name: "Task 2"
+    },
+    {
+      id: 3,
+      name: "Task 3"
+    },
+    {
+      id: 4,
+      name: "Task 4"
+    }
+  ];
+}
